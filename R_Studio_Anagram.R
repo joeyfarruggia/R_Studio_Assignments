b10df059f2ce450bd38b62024e8ac417b767ca52
Joey Farruggia <joeyfarruggia66@gmail.com>
2026-08-19 16:22:49 -0700

    Rewrite is_anagram function in R
    Converted Python anagram function to R syntax.

--- R_Studio_Anagram.R
+++ R_Studio_Anagram.R
@@ -1,5 +1,9 @@
-def is_anagram(str1, str2):
-  return sorted(str1.lower()) == sorted(str2.lower())
+is_anagram <- function(str1, str2) {
+  chars1 <- sort(strsplit(tolower(str1), "")[[1]])
+  chars2 <- sort(strsplit(tolower(str2), "")[[1]])
+
+  identical(chars1, chars2)
+}
 
 print(is_anagram("listen", "silent"))
-#Output should be true
+# Expected output: TRUE
